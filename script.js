1ea4c664e41671c75c67fe5e75efee1f20331dd2
Danimko <extaz.showroom@yandex.ru>
2022-07-01 18:22:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const button1 = document.querySelector('.galery__tab--1');
+const button2 = document.querySelector('.galery__tab--2');
+const button3 = document.querySelector('.galery__tab--3');
+const button4 = document.querySelector('.galery__tab--4');
+const tab1 = document.querySelector('.galery__text--1');
+const tab2 = document.querySelector('.galery__text--2');
+const tab3 = document.querySelector('.galery__text--3');
+const tab4 = document.querySelector('.galery__text--4');
+
+
+
+button1.addEventListener('click', function () {
+
+    tab2.classList.add('galery__text--none');
+    tab3.classList.add('galery__text--none');
+    tab4.classList.add('galery__text--none');    
+    tab1.classList.remove('galery__text--none');    
+
+    
+});
+
+button2.addEventListener('click', function () {
+
+    tab1.classList.add('galery__text--none');
+    tab3.classList.add('galery__text--none');
+    tab4.classList.add('galery__text--none');    
+    tab2.classList.remove('galery__text--none');    
+
+    
+});
+
+button3.addEventListener('click', function () {
+
+    tab1.classList.add('galery__text--none');
+    tab2.classList.add('galery__text--none');
+    tab4.classList.add('galery__text--none');    
+    tab3.classList.remove('galery__text--none');    
+
+    
+});
+
+button4.addEventListener('click', function () {
+
+    tab1.classList.add('galery__text--none');
+    tab3.classList.add('galery__text--none');
+    tab2.classList.add('galery__text--none');    
+    tab4.classList.remove('galery__text--none');    
+
+    
+});
